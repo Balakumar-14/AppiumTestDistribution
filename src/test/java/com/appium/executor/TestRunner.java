59cb86f569832e51d81026f7700e3d41c86a74db
Jeeva <azhagusundaram.t@zohocorp.com>
2023-07-14 13:34:36 +0530

    Commented all inbuilt tests

--- src/test/java/com/appium/executor/TestRunner.java
+++ src/test/java/com/appium/executor/TestRunner.java
@@ -5,19 +5,23 @@ import org.testng.annotations.Test;
 
 public class TestRunner {
 
-    @Test public void testMethod1() {
+    // @Test
+    public void testMethod1() {
         System.err.println("******RunnerTestMethod1****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod2() {
+    // @Test
+    public void testMethod2() {
         System.err.println("******RunnerTestMethod2****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod3() {
+    // @Test
+    public void testMethod3() {
         System.err.println("******RunnerTestMethod3****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod4() {
+    // @Test
+    public void testMethod4() {
         System.err.println("******RunnerTestMethod4****" + Thread.currentThread().getId());
     }
 
--- src/test/java/com/appium/executor/TestRunner2.java
+++ src/test/java/com/appium/executor/TestRunner2.java
@@ -4,19 +4,23 @@ import org.testng.annotations.Test;
 
 public class TestRunner2 {
 
-    @Test public void testMethod1() {
+    // @Test
+    public void testMethod1() {
         System.err.println("******RunnerTest1Method1****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod2() {
+    // @Test
+    public void testMethod2() {
         System.err.println("******RunnerTest1Method2****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod3() {
+    // @Test
+    public void testMethod3() {
         System.err.println("******RunnerTest1Method3****" + Thread.currentThread().getId());
     }
 
-    @Test public void testMethod4() {
+    // @Test
+    public void testMethod4() {
         System.err.println("******Runner1TestMethod4****" + Thread.currentThread().getId());
     }
 
